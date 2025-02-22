1eda287c3da2465d0c0d29180bd44f64cf000cb2
VoinBoria <voinboria@gmail.com>
2025-02-22 18:07:44 +0200

    Update ReminderWorker.kt

--- ReminderWorker.kt
+++ ReminderWorker.kt
@@ -1,11 +1,7 @@
 package com.serhio.homeaccountingapp
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
+import android.content.Intent
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
@@ -18,44 +14,12 @@ class ReminderWorker(
         val taskTitle = inputData.getString("TASK_TITLE")
         val reminderTime = inputData.getString("REMINDER_TIME")
 
-        showNotification(taskTitle, reminderTime)
+        val intent = Intent(applicationContext, ReminderBroadcastReceiver::class.java).apply {
+            putExtra("TASK_TITLE", taskTitle)
+            putExtra("REMINDER_TIME", reminderTime)
+        }
+        applicationContext.sendBroadcast(intent)
 
         return Result.success()
-    }
-
-    private fun showNotification(taskTitle: String?, reminderTime: String?) {
-        val channelId = "task_reminder_channel"
-        val channelName = "Task Reminder"
-        val importance = NotificationManager.IMPORTANCE_HIGH
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(channelId, channelName, importance).apply {
-                description = "Канал для нагадувань про задачі"
-            }
-            val notificationManager: NotificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-
-        val message = if (reminderTime == "на початку") {
-            applicationContext.getString(R.string.task_start_message, taskTitle)
-        } else {
-            applicationContext.getString(R.string.task_reminder_message, taskTitle, reminderTime)
-        }
-
-        val builder = NotificationCompat.Builder(applicationContext, channelId)
-            .setSmallIcon(R.drawable.ic_notification)
-            .setContentTitle(applicationContext.getString(R.string.reminder))
-            .setContentText(message)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setAutoCancel(true)
-
-        try {
-            with(NotificationManagerCompat.from(applicationContext)) {
-                notify((System.currentTimeMillis() and 0xFFFFFFF).toInt(), builder.build())
-            }
-        } catch (e: SecurityException) {
-            // Handle the SecurityException if permission is denied
-            e.printStackTrace()
-        }
     }
 }
